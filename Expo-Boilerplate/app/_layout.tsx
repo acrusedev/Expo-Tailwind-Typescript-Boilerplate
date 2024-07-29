f214c375b92b1aa7825e0a420c8f5f0472d7de94
acrusedev <nominos09@gmail.com>
2024-07-30 01:33:37 +0200

    remove unnecessary files

--- Expo-Boilerplate/app/_layout.tsx
+++ Expo-Boilerplate/app/_layout.tsx
@@ -1,9 +1,13 @@
+// initial client file that will be rendered on the client side
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen 
+      name="index" 
+      options={{ title: "Home" }}
+      />
     </Stack>
   );
 }
--- Expo-Boilerplate/app/index.tsx
+++ Expo-Boilerplate/app/index.tsx
@@ -8,7 +8,7 @@ const StyledText = styled(Text);
 
 export default function HomeScreen() {
   return (
-    <StyledView className="flex-1 items-center justify-center bg-white">
+    <StyledView className="flex-auto items-center justify-center">
       <StyledText className='text-3xl'>Open up App.js to start working on your app!</StyledText>
     </StyledView>
   );
